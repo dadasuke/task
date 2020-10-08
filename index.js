3962e3dc7fd939ca38075628b61a0f0ecf2be250
dadasuke <46437491+dadasuke@users.noreply.github.com>
2020-10-08 18:47:44 +0900

    a

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Home = { template: `<h1 class="title">ゆめみてる</h1><h2 class="subtitle">サブタイトル</h2>` }
+const Movie = { template: `<h1 class="title">動画コンテンツ</h1><div class="block"><video src="assets/movies/cat.mp4" controls preload="none"></video></div>`}
 const Company = { template: `<h1 class="title">企業情報</h1>`}
 const Store = { template: `<h1 class="title">店舗案内</h1>`}
 const Recruit = { template: `<h1 class="title">求人情報</h1>`}
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/Movie', component: Movie },
   { path: '/Company', component: Company },
   { path: '/Store', component: Store },
   { path: '/Recruit', component: Recruit }
